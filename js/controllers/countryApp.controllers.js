7de1462ff202f837a9337da2ee16120aac119567
adrianprzybyla <adrian.przybyla@o2.pl>
2017-02-17 15:37:33 +0100

    Update countryApp.controllers.js

--- js/controllers/countryApp.controllers.js
+++ js/controllers/countryApp.controllers.js
@@ -2,77 +2,76 @@ angular
     .module('countryApp.controllers', [])
     .controller('listController', listController)
 
-function listController(listFactory, $scope) {
+    function listController (listFactory) {
+        
+        var vm = this;
 
-	$scope.continents = [
-	    {name: 'AFRICA', countrys: [], selected: false}, 
-	    {name: 'AMERICAS', countrys: [], selected: false},
-	    {name: 'ASIA', countrys: [], selected: false},
-	    {name: 'EUROPE', countrys: [], selected: false},
-	    {name: 'OCEANIA', countrys: [], selected: false},
-	    {name: 'POLAR', countrys: [], selected: false}
-    ];
+        vm.continents = [
+            {name: 'AFRICA', countrys: [], selected: false}, 
+            {name: 'AMERICAS', countrys: [], selected: false},
+            {name: 'ASIA', countrys: [], selected: false},
+            {name: 'EUROPE', countrys: [], selected: false},
+            {name: 'OCEANIA', countrys: [], selected: false},
+            {name: 'POLAR', countrys: [], selected: false}
+        ];
 
-    listFactory.query(function (data) {
-        for(var i = 0; i < data.length; i++) {
-            var obj = data[i],
-                newObj = {name: obj.name, region: obj.region, checked: false}    
-            for(var j = 0; j < $scope.continents.length; j++) {
-                if($scope.continents[j].name.toLowerCase() === newObj.region.toLowerCase()) {
-                    $scope.continents[j].countrys.push(newObj)
+        listFactory.query(data => {
+            for(let i = 0; i < data.length; i++) {
+                const obj = data[i],
+                    newObj = {name: obj.name, region: obj.region, checked: false}    
+                for(let j = 0; j < vm.continents.length; j++) {
+                    if(vm.continents[j].name.toLowerCase() === newObj.region.toLowerCase()) {
+                        vm.continents[j].countrys.push(newObj)
+                    }
                 }
+            } 
+        })
+
+        vm.checkAll = continent => {
+            if(continent.selected) {
+                for(let i = 0; i < vm.continents.length; i++) {
+                    if(vm.continents[i].name.toLowerCase() === continent.name.toLowerCase()) {
+                        vm.continents[i].countrys.forEach(elem => {
+                            elem.checked = true;
+                        })
+                    }
+                }
+            } else if (!continent.selected) {
+                for(let i = 0; i < vm.continents.length; i++) {
+                    if(vm.continents[i].name.toLowerCase() === continent.name.toLowerCase()) {
+                        vm.continents[i].countrys.forEach(elem => {
+                            elem.checked = false;
+                        })
+                    }
+                }           
             }
-        } 
-    })
-
-    $scope.checkAll = function (continent) {
-        if(continent.selected) {
-            for(var i = 0; i < $scope.continents.length; i++) {
-                if($scope.continents[i].name.toLowerCase() === continent.name.toLowerCase()) {
-                    $scope.continents[i].countrys.forEach(function (elem) {
-                        elem.checked = true;
-                    })
-                }
-            }
-        } else if (!continent.selected) {
-            for(var i = 0; i < $scope.continents.length; i++) {
-                if($scope.continents[i].name.toLowerCase() === continent.name.toLowerCase()) {
-                    $scope.continents[i].countrys.forEach(function (elem) {
-                        elem.checked = false;
-                    })
-                }
-            }           
         }
+
+        vm.checkContinentIfAllChecked = country => {
+
+            let continent,
+                index,
+                bool;
+
+            for(let i = 0; i < vm.continents.length; i++) {
+                if(vm.continents[i].name.toLowerCase() == country.region.toLowerCase()) {
+                    index = i
+                    continent = vm.continents[index]
+                }
+            }
+
+            bool = continent.countrys.every(element => {
+                if(element.checked === true) {
+                    return true;
+                } else {
+                    return false;
+                }
+            })
+
+            if (bool) {
+                continent.selected = true;
+            } else if (!bool) {
+                continent.selected = false;
+            }
+        }   
     }
-	$scope.changeIfAllChecked = function (country) {
-
-		var continent,
-			index,
-			bool;
-
-		for(var i = 0; i < $scope.continents.length; i++) {
-			if($scope.continents[i].name.toLowerCase() == country.region.toLowerCase()) {
-				index = i
-				continent = $scope.continents[index]
-			}
-		}
-
-		bool = continent.countrys.every(function (element) {
-			if(element.checked === true) {
-				return true;
-			} else {
-				return false;
-			}
-		})
-
-		if(bool) {
-			continent.selected = true;
-		} else if(!bool){
-			continent.selected = false;
-		}
-	}	
-
-}
-
-
-
